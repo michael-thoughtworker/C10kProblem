c13d6416b264b53bce50a3f283e3c98ca8665b93
michael.li <michael.li@thoughtworks.com>
2022-08-18 13:35:13 +0800

    fix vagrant issue

--- vagrant/scripts/install-bcc.sh
+++ vagrant/scripts/install-bcc.sh
@@ -4,7 +4,7 @@ set -eux
 
 sudo apt-get update
 sudo apt-get install -y bpfcc-tools "linux-headers-$(uname -r)"
-sudo apt-get install sysstat
-sudo apt-get install maven
-sudo apt-get install git
+sudo apt-get install -y sysstat
+sudo apt-get install -y maven
+sudo apt-get install -y git
 
