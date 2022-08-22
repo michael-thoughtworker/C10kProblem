6444fea84588b6ec04ae7cdd718b695a382aece8
michael.li <michael.li@thoughtworks.com>
2022-08-22 16:54:40 +0800

    fix vagrant issue

--- vagrant/scripts/install-bcc.sh
+++ vagrant/scripts/install-bcc.sh
@@ -7,4 +7,7 @@ sudo apt-get install -y bpfcc-tools "linux-headers-$(uname -r)"
 sudo apt-get install -y sysstat
 sudo apt-get install -y maven
 sudo apt-get install -y git
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 
